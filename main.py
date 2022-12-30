63ffde8247533d0ff0bfaff377fbc964ac79ecb8
sjcahill <sjoscahill@gmail.com>
2022-12-29 20:17:27 -0500

    add logging functionality

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import logging
+
+
 if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    mydict: dict = dict()
+    try:
+        mydict["missing_key"]
+    except KeyError as e:
+        logging.info(e)
 
     print("toot!")
     print("toot!")
